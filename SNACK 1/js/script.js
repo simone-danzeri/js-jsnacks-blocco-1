74f356bae33fa1d78671942b1ccb72f2598df698
simone-danzeri <s.danzeri@icloud.com>
2024-03-11 13:22:41 +0100

    feat: SNACK 1 - Complete

--- SNACK 1/js/script.js
+++ SNACK 1/js/script.js
@@ -1,9 +1,16 @@
 // Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati (inserire il numero nell'array solo se non è già presente).
-
+// Mi serve array vuoto di partenza nel quale metterò i numeri che mi utente
+const numbersArray = [];
+console.log(numbersArray);
 // Chiedo all'utente i 5 numeri
 let userNumbers
 for (let i = 0; i < 5; i++) {
     userNumbers = parseInt(prompt('Ciao! Dimmi un numero'));
     console.log(userNumbers);
-    
+
+    if (numbersArray.includes(userNumbers) === false) {
+        numbersArray.push(userNumbers);
+    }
 }
+
+console.log(numbersArray);
