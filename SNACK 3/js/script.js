dbbd81895588d0e2d822ae0c93727f01b5e9de76
simone-danzeri <s.danzeri@icloud.com>
2024-03-11 14:05:59 +0100

    feat: SNACK 3 - Complete

--- SNACK 3/js/script.js
+++ SNACK 3/js/script.js
@@ -2,9 +2,19 @@
 // Chiedi per 6 volte all’utente di inserire un numero,
 // se è dispari inseriscilo nell’array.
 
+// Mi creo array vuoto
+const numbersArray = [];
+console.log(numbersArray);
 // Chiedo 6 numeri a utente
 let userNumbers;
 for (let i = 0; i < 6; i++) {
     userNumbers = parseInt(prompt('Ciao! Dimmi un numero'));
     console.log(userNumbers);
+    // Controllo se è dispari
+    if (userNumbers % 2 !== 0) {
+        // Lo inserisco in array
+        numbersArray.push(userNumbers);
+    }
 }
+// Stampo in console l'array
+console.log(numbersArray);
