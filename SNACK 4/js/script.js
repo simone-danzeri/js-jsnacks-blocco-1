8949007e6337541ffaef98a7f160f6b189604afa
simone-danzeri <s.danzeri@icloud.com>
2024-03-11 14:14:27 +0100

    feat: SNACK 4 : add sum first 10 numbers

---
+++ SNACK 4/js/script.js
@@ -0,0 +1,11 @@
+// Calcola la somma e la media dei primi 10 numeri.
+
+let sum = 0;
+// Prendo i primi 10 numeri
+for (let i = 1; i < 11 ; i++) {
+    console.log(i);
+    // Per ogni ciclo devo sommare i a se stessa
+    sum = sum + i;
+}
+
+console.log(sum);
