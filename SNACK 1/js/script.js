d8a7da96bf3e11de07bccacb60d51d0afc3bddb2
simone-danzeri <s.danzeri@icloud.com>
2024-03-11 13:14:23 +0100

    first commit

---
+++ SNACK 1/js/script.js
@@ -0,0 +1,8 @@
+// Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati (inserire il numero nell'array solo se non è già presente).
+
+// Chiedo all'utente i 5 numeri
+let userNumbers
+for (let i = 0; i < 5; i++) {
+    userNumbers = parseInt(prompt('Ciao! Dimmi un numero'));
+    console.log(userNumbers);
+}
