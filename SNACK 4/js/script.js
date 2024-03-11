958372083401c40717f9a364a34503f3ed73c911
simone-danzeri <s.danzeri@icloud.com>
2024-03-11 14:20:38 +0100

    feat: SNACK 4 : add arithmetic mean

--- SNACK 4/js/script.js
+++ SNACK 4/js/script.js
@@ -1,11 +1,15 @@
 // Calcola la somma e la media dei primi 10 numeri.
 
 let sum = 0;
+let mean;
 // Prendo i primi 10 numeri
 for (let i = 1; i < 11 ; i++) {
     console.log(i);
     // Per ogni ciclo devo sommare i a se stessa
     sum = sum + i;
+    // Calcolo la media
+    mean = sum / i;
 }
-
+// Stampo somma e media in console
 console.log(sum);
+console.log(mean);
