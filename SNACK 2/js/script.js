839889708c29a4b058ef83c4227940f59b475d02
simone-danzeri <s.danzeri@icloud.com>
2024-03-11 13:32:11 +0100

    feat: SNACK 2 - Complete

--- SNACK 2/js/script.js
+++ SNACK 2/js/script.js
@@ -1,8 +1,13 @@
 //  Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
 
 // Chiedo 5 numeri a utente
-let userNumbers
+let userNumbers;
+let sum = 0;
 for (let i = 0; i < 5; i++) {
     userNumbers = parseInt(prompt('Ciao! Dimmi un numero'));
     console.log(userNumbers);
+    // Sommo i 5 numeri tra di loro
+    sum = sum + userNumbers;
 }
+// Stampo la somma in console
+console.log(sum);
